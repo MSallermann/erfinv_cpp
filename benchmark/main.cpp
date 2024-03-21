070a3347ab3ecfe1476eef971028ba962141e6e4
Moritz Sallermann <moritzsallermann@gmail.com>
2024-03-21 17:53:58 +0000

    Misc: Adjusted verification and benchmark

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,13 +1,14 @@
 #include "erfinv.hpp"
 #include "nanobench.h"
-#include <iostream>
 
 // NOLINTNEXTLINE
 
 int main()
 {
-    double x       = 0.5;
-    double erfinvx = erfinv::erfinv( x );
+    using scalar = double;
+
+    scalar x       = 0.5;
+    scalar erfinvx = erfinv::erfinv( x );
 
     auto bench = ankerl::nanobench::Bench().minEpochIterations( 1000000 );
 
@@ -16,57 +17,61 @@ int main()
     } );
 
     bench.run( "erfinv_series_1", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 1>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 1>( x ) );
     } );
 
     bench.run( "erfinv_series_2", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 2>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 2>( x ) );
     } );
 
     bench.run( "erfinv_series_3", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 3>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 3>( x ) );
     } );
 
     bench.run( "erfinv_series_4", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 4>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 4>( x ) );
     } );
 
     bench.run( "erfinv_series_5", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 5>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 5>( x ) );
     } );
 
     bench.run( "erfinv_series_10", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<double, 10>( x ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_series<scalar, 10>( x ) );
     } );
 
     bench.run( "erfinv_newton_1", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<double, 1>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<scalar, 1>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv_newton_2", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<double, 2>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<scalar, 2>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv_newton_3", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<double, 3>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_newton<scalar, 3>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv_halley_1", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<double, 1>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<scalar, 1>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv_halley_2", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<double, 2>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<scalar, 2>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv_halley_3", [&]() {
-        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<double, 3>( x, erfinvx ) );
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_halley<scalar, 3>( x, erfinvx ) );
     } );
 
     bench.run( "erfinv", [&]() { ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::erfinv( x ) ); } );
 
-    double erferfinvx = 0;
-    bench.run( "erf", [&]() { ankerl::nanobench::doNotOptimizeAway( erferfinvx = std::erf( erfinvx ) ); } );
+    bench.run( "erfinv_golang", [&]() {
+        ankerl::nanobench::doNotOptimizeAway( erfinvx = erfinv::detail::erfinv_golang_math( x ) );
+    } );
+
+    scalar erferfinvx = 0;
+    bench.run( "erfl", [&]() { ankerl::nanobench::doNotOptimizeAway( erferfinvx = std::erfl( erfinvx ) ); } );
 
     // bench.render( ankerl::nanobench::templates::csv(), std::cout );
     // bench.render( ankerl::nanobench::templates::htmlBoxplot(), std::cout );
--- verification/main.cpp
+++ verification/main.cpp
@@ -1,18 +1,19 @@
 #include "cmath"
 #include "erfinv.hpp"
-#include "iostream"
 #include <cmath>
 #include <fstream>
 #include <iomanip>
 #include <limits>
 
+using scalar = double;
+
 template<typename erfinv_func_t>
-void report( double x, erfinv_func_t erfinv, std::ofstream & fstream )
+void report( scalar x, erfinv_func_t erfinv, std::ofstream & fstream )
 {
-    auto inv      = erfinv( x );
-    auto erf_inv  = std::erf( inv );
-    auto rel_err  = std::max( std::abs( ( erf_inv - x ) / x ), std::numeric_limits<double>::epsilon() );
-    auto n_digits = -std::log10( rel_err );
+    scalar inv      = erfinv( x );
+    scalar erf_inv  = std::erfl( inv );
+    scalar rel_err  = std::max( std::abs( ( erf_inv - x ) / x ), std::numeric_limits<scalar>::epsilon() );
+    scalar n_digits = -std::log10( rel_err );
 
     fstream << std::setprecision( 16 );
     fstream << x << " " << inv << " " << erf_inv << " " << n_digits << "\n";
@@ -25,7 +26,7 @@ void run_function_on_samples( const std::string & filename, erfinv_func_t erfinv
     int N_Samples = 10000;
     for( int i = 0; i < N_Samples; i++ )
     {
-        double x = -1.0 + 2.0 * i / ( N_Samples - 1.0 );
+        scalar x = -1.0 + 2.0 * i / ( N_Samples - 1.0 );
         report( x, erfinv, file_out );
     }
     file_out.close();
@@ -33,27 +34,24 @@ void run_function_on_samples( const std::string & filename, erfinv_func_t erfinv
 
 int main()
 {
-    run_function_on_samples( "series_5.txt", erfinv::detail::erfinv_series<double, 5> );
-    run_function_on_samples( "series_10.txt", erfinv::detail::erfinv_series<double, 10> );
-    run_function_on_samples( "winitzki.txt", erfinv::detail::erfinv_winitzki<double> );
-    run_function_on_samples( "full.txt", erfinv::erfinv<double> );
+    run_function_on_samples( "series_5.txt", erfinv::detail::erfinv_series<scalar, 5> );
+    run_function_on_samples( "series_10.txt", erfinv::detail::erfinv_series<scalar, 10> );
+    run_function_on_samples( "winitzki.txt", erfinv::detail::erfinv_winitzki<scalar> );
+    run_function_on_samples( "full.txt", erfinv::erfinv<scalar> );
+    run_function_on_samples( "erfinv_golang.txt", erfinv::detail::erfinv_golang_math<scalar> );
 
     using erfinv::detail::erfinv_series;
     using erfinv::detail::erfinv_winitzki;
 
-    run_function_on_samples( "win_newton.txt", []( double x ) {
-        return erfinv::detail::erfinv_newton<double, 1>( x, erfinv_winitzki( x ) );
+    run_function_on_samples( "win_2halley.txt", []( scalar x ) {
+        return erfinv::detail::erfinv_halley<scalar, 2>( x, erfinv_winitzki( x ) );
     } );
 
-    run_function_on_samples( "win_2halley.txt", []( double x ) {
-        return erfinv::detail::erfinv_halley<double, 2>( x, erfinv_winitzki( x ) );
+    run_function_on_samples( "series_10_1halley.txt", []( scalar x ) {
+        return erfinv::detail::erfinv_halley<scalar, 1>( x, erfinv_series<scalar, 10>( x ) );
     } );
 
-    run_function_on_samples( "series_10_1halley.txt", []( double x ) {
-        return erfinv::detail::erfinv_halley<double, 1>( x, erfinv_series<double, 10>( x ) );
-    } );
-
-    run_function_on_samples( "series_10_2halley.txt", []( double x ) {
-        return erfinv::detail::erfinv_halley<double, 2>( x, erfinv_series<double, 10>( x ) );
+    run_function_on_samples( "series_10_2halley.txt", []( scalar x ) {
+        return erfinv::detail::erfinv_halley<scalar, 2>( x, erfinv_series<scalar, 10>( x ) );
     } );
 }
